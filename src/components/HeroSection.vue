14c138743077fc4d4540aa30415dfa0bc9221890
Johannes Kärnstam <johannes.karnstam@vgregion.se>
2026-03-26 12:37:58 +0100

    Minor fix

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -41,7 +41,15 @@ const { t } = useI18n();
 
     <!-- Scroll Indicator -->
     <div class="absolute bottom-10 left-1/2 -translate-x-1/2 flex flex-col items-center opacity-40 animate-bounce">
-      <div class="w-[1px] h-12 bg-offwhite mb-2"></div>
+      <svg 
+        class="w-12 h-12 text-offwhite" 
+        fill="none" 
+        stroke="currentColor" 
+        viewBox="0 0 24 24" 
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+      </svg>
     </div>
   </section>
 </template>
